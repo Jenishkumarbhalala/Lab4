45ca3ce96f4957ff1d2ec22f11a107dd6185d51e
JenishKumar Bhalala <Jbhalala8191@conestogac.on.ca>
2024-02-19 13:08:01 -0500

    Add project files.

---
+++ NUnitDemonstration/TriangleTests.cs
@@ -0,0 +1,67 @@
+﻿using NUnit.Framework;
+
+namespace NUnitDemonstration
+{
+    public class TriangleTests
+    {
+        [Test]
+        public void ValidTriangle_ValidInput_ReturnsValidMessage()
+        {
+            // Arrange
+            int firstAngle = 60;
+            int secondAngle = 60;
+            int thirdAngle = 60;
+
+            // Act
+            string result = Triangle.ValidTriangle(firstAngle, secondAngle, thirdAngle);
+
+            // Assert
+            Assert.AreEqual("The triangle is valid.", result);
+        }
+
+        [Test]
+        public void ValidTriangle_InvalidInput_ReturnsInvalidMessage()
+        {
+            // Arrange
+            int firstAngle = 90;
+            int secondAngle = 45;
+            int thirdAngle = 45;
+
+            // Act
+            string result = Triangle.ValidTriangle(firstAngle, secondAngle, thirdAngle);
+
+            // Assert
+            Assert.AreEqual("The triangle is NOT valid.", result);
+        }
+
+        [Test]
+        public void ValidTriangle_Equilateral_ReturnsValidMessage()
+        {
+            // Arrange
+            int firstAngle = 60;
+            int secondAngle = 60;
+            int thirdAngle = 60;
+
+            // Act
+            string result = Triangle.ValidTriangle(firstAngle, secondAngle, thirdAngle);
+
+            // Assert
+            Assert.AreEqual("The triangle is valid.", result);
+        }
+
+        [Test]
+        public void ValidTriangle_InvalidSum_ReturnsInvalidMessage()
+        {
+            // Arrange
+            int firstAngle = 90;
+            int secondAngle = 60;
+            int thirdAngle = 30;
+
+            // Act
+            string result = Triangle.ValidTriangle(firstAngle, secondAngle, thirdAngle);
+
+            // Assert
+            Assert.AreEqual("The triangle is NOT valid.", result);
+        }
+    }
+}
